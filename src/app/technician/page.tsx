befe16e8d53640394b176783934cb3b272f59c8a
Ruben Dario Adorno <dario.adorno@gmail.com>
2026-02-24 17:21:11 -0300

    UI: Remove drop-shadow from tecnico to fix border, translate down further

--- src/app/technician/page.tsx
+++ src/app/technician/page.tsx
@@ -64,11 +64,11 @@ export default function TechnicianPage() {
                 title={userName ? `Hola, ${userName}` : "Cargando..."}
                 subtitle="Gestiona tus revisiones de EPP"
                 icon={
-                    <div className="w-16 h-16 sm:w-20 sm:h-20 shrink-0 relative mr-2">
+                    <div className="w-16 h-16 sm:w-20 sm:h-20 shrink-0 relative mr-2 translate-y-2">
                         <img
                             src="/tecnico.png"
                             alt="Técnico"
-                            className="absolute bottom-0 w-full object-contain drop-shadow-md"
+                            className="absolute bottom-0 w-full object-contain"
                             style={{ height: '140%' }} // Make it slightly taller than its container but anchored to the bottom
                         />
                     </div>
